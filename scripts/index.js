bddfdeba700e0edb74695997facec82addbbc933
Daniel <83757480+DeeAyDan@users.noreply.github.com>
2023-07-19 17:42:41 +0200

    Added playGame function

--- scripts/index.js
+++ scripts/index.js
@@ -14,3 +14,44 @@ function getComputerChoice(){
 
 const computerSelection = getComputerChoice();
 const playerSelection = "rock";
+
+function playRound(player, computer){
+    if(player === computer){
+        console.log(`It's a tie! ${player} can't beat ${computer}.`);
+    }
+    else if(computer === "rock"){
+        switch(player){
+            case "paper":
+                console.log(`You win! ${player} beats ${computer}.`);
+                break;
+            case "scissors":
+                console.log(`You Lose! ${player} is inferior to ${computer}.`);
+                break;
+        }
+    }
+    else if(computer === "paper"){
+        switch(player){
+            case "scissors":
+                console.log(`You win! ${player} beats ${computer}.`);
+                break;
+            case "rock":
+                console.log(`You Lose! ${player} is inferior to ${computer}.`);
+                break;
+        }
+    }
+    else if(computer === "scissors"){
+        switch(player){
+            case "rock":
+                console.log(`You win! ${player} beats ${computer}.`);
+                break;
+            case "paper":
+                console.log(`You Lose! ${player} is inferior to ${computer}.`);
+                break;
+        }
+    }
+
+    console.log(`Player: ${player}`);
+    console.log(`CPU: ${computer}`);
+}
+
+playRound(playerSelection,computerSelection);
